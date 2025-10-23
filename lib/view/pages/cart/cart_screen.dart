3ad63fa4e4084009f7131b87e8070cf524406490
Your Name <you@example.com>
2025-10-23 20:53:03 +0600

    add cart page

--- lib/view/pages/cart/cart_screen.dart
+++ lib/view/pages/cart/cart_screen.dart
@@ -1,4 +1,6 @@
+import 'package:e_commerce/controllers/cart_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -8,17 +10,166 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  CartController _cartController = Get.put(CartController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Cart"),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text("Cart Screen"),
+      appBar: AppBar(title: const Text("Cart"), centerTitle: true),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Obx(
+              () => ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) => Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xffDEDEDE),
+                  ),
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(
+                          _cartController
+                              .myCartList
+                              .value[index]['product_image'],
+                          height: 100,
+                          width: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(width: 15),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              _cartController
+                                  .myCartList
+                                  .value[index]['product_name'],
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              "\$${_cartController.myCartList[index]['product_price']} (-\$${_cartController.myCartList[index]['product_tax']} Tax)",
+                              style: TextStyle(
+                                fontSize: 11,
+                                color: Color(0xff8F959E),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+
+                            Row(
+                              children: [
+                                if (_cartController
+                                        .myCartList[index]['stock'] !=
+                                    0)
+                                  GestureDetector(
+                                    onTap: () {
+                                      _cartController.decrement(index);
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.all(5),
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+
+                                        border: Border.all(
+                                          color: Color(0xff8F959E),
+                                        ),
+                                      ),
+                                      child: Icon(Icons.keyboard_arrow_down),
+                                    ),
+                                  ),
+                                SizedBox(width: 10),
+                                _cartController.myCartList[index]['stock'] != 0
+                                    ? Text(
+                                        _cartController
+                                            .myCartList
+                                            .value[index]['quantity']
+                                            .toString(),
+                                      )
+                                    : Text(
+                                        "Out of Stock",
+                                        style: TextStyle(color: Colors.red),
+                                      ),
+                                SizedBox(width: 10),
+                                if (_cartController
+                                        .myCartList[index]['stock'] !=
+                                    0)
+                                  GestureDetector(
+                                    onTap: () {
+                                      _cartController.increment(index);
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.all(5),
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+
+                                        border: Border.all(
+                                          color: Color(0xff8F959E),
+                                        ),
+                                      ),
+                                      child: Icon(Icons.keyboard_arrow_up),
+                                    ),
+                                  ),
+
+                                SizedBox(width: 10),
+                                Spacer(),
+                                GestureDetector(
+                                  onTap: () {
+                                    _cartController.delete(index);
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+
+                                      border: Border.all(
+                                        color: Color(0xff8F959E),
+                                      ),
+                                    ),
+                                    child: Icon(Icons.delete_outline),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                separatorBuilder: (context, index) => SizedBox(height: 10),
+                itemCount: _cartController.myCartList.length,
+              ),
+            ),
+
+            SizedBox(height: 10),
+            ListTile(
+              dense: true,
+              horizontalTitleGap: 0,
+              contentPadding: EdgeInsets.zero,
+              leading: Image.asset(
+                "assets/images/product_image1.png",
+                height: 300,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
       ),
     );
-();
+    ();
   }
 }
